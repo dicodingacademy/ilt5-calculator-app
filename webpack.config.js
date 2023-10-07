cd158875363f358b0bdf18c6db9c637315568ff4
Nur Rizki Adi Prasetyo <nrizki@dicoding.com>
2023-10-07 10:09:01 +0700

    Update libraries version

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: {
     app: path.resolve(__dirname, 'src/scripts/index.js'),
   },
   output: {
-    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
     clean: true,
   },
   module: {
@@ -20,6 +20,7 @@ module.exports = {
     ],
   },
   devServer: {
+    open: true,
     static: path.resolve(__dirname, 'dist'),
     watchFiles: [
       path.resolve(__dirname, 'src/**/*'),
@@ -33,6 +34,8 @@ module.exports = {
     },
   },
   plugins: [
+    new CleanWebpackPlugin(),
+
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: path.resolve(__dirname, 'index.html'),
